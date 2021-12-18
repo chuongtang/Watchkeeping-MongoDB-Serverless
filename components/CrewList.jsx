f2465c9e38c8b5ae8708a0716b890737f55cc68c
chuongtang <chuong.tang@aol.com>
2021-12-18 11:21:10 -0700

    successful render crewlist

--- components/CrewList.jsx
+++ components/CrewList.jsx
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import dotenv from 'dotenv';
-dotenv.config();
+
+// dotenv/config();
 import * as Realm from "realm-web";
 
 
 const CrewList = () => {
-  const REALM_APP_ID = import.meta.env.VITE_REALM_APP_ID;
-
-  const [crewList, setCrewList] = useState([]);
+  const REALM_APP_ID = "shiptime_backend-zjsnd";
+  // const REALM_APP_ID = import.meta.env.VITE_REALM_APP_ID;
+  console.log(REALM_APP_ID);
+  console.log("dotHERE", dotenv.config)
+  const [crews, setCrews] = useState([]);
   const columns = [
-    { field: 'fullname', header: 'Full Name' },
-    { field: 'rank', header: 'Rank' },
-    { field: 'watchkepeer', header: 'Watchkeeper' },
-    { field: 'nationality', header: 'Nationality' }
+    { field: 'Fullname', header: 'Full Name' },
+    { field: 'Rank', header: 'Rank' },
+    { field: 'Watchkeeper', header: 'Watchkeeper' },
+    { field: 'Nationality', header: 'Nationality' }
   ];
 
   useEffect(async () => {
-    
+
     const app = new Realm.App({ id: REALM_APP_ID });
 
     // this will give user anonymous access.
@@ -26,25 +29,39 @@ const CrewList = () => {
 
     try {
       const user = await app.logIn(credentials);
-      const crews = await user.functions.FetchCrewList();
-      setCrewList(crews);
+      const crewList = await user.functions.FetchCrewList();
+      setCrews(crewList);
+      console.log("HereISCrewList",crewList[0]);
     } catch (error) {
       console.error(error);
     }
   }, []);
 
+  const booleanChecker = (rowData, item) => {
+    if (typeof rowData[item.field] === "boolean") {
+      return ((rowData[item.field]) ? "Accepted" : "Unaccepted");
+    } else {
+      return rowData[item.field];
+    }
+  };
+
   const dynamicColumns = columns.map((col, i) => {
-    return <Column key={col.field} field={col.field} header={col.header} />;
+    return <Column 
+        key={col.field} 
+        field={col.field} 
+        header={col.header}
+        body={booleanChecker} />;
   });
 
   return (
     <div>
-    <div className="card">
-        <DataTable value={crewList} responsiveLayout="scroll">
-            {dynamicColumns}
+      <div className="card">
+        <DataTable value={crews} responsiveLayout="scroll">
+          {dynamicColumns}
         </DataTable>
+
+      </div>
     </div>
-</div>
   )
 };
 export default CrewList
