d0a71f29910beb92596a60e712181b60c3eec081
chuongtang <chuong.tang@aol.com>
2021-12-18 12:30:28 -0700

    successfully render scrollable table

--- components/CrewList.jsx
+++ components/CrewList.jsx
@@ -2,23 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import dotenv from 'dotenv';
+import { Button } from 'primereact/button';
 
 // dotenv/config();
 import * as Realm from "realm-web";
 
 
 const CrewList = () => {
-  // const REALM_APP_ID = "shiptime_backend-zjsnd";
+  
   const REALM_APP_ID = import.meta.env.VITE_REALM_APP_ID;
-  console.log(REALM_APP_ID);
-  console.log("dotHERE", dotenv.config)
+ 
   const [crews, setCrews] = useState([]);
+  const [crew, setCrew] = useState([]);
+  const [crewDialog, setCrewDialog] = useState(false);
+
   const columns = [
     { field: 'Fullname', header: 'Full Name' },
     { field: 'Rank', header: 'Rank' },
     { field: 'Watchkeeper', header: 'Watchkeeper' },
     { field: 'Nationality', header: 'Nationality' }
   ];
+
+  const editcrew = (crew) => {
+    setCrew({...crew});
+    setCrewDialog(true);
+}
+
+  const actionBodyTemplate = (rowData) => {
+    return (
+        <React.Fragment>
+            <Button icon="pi pi-pencil" className="p-button-rounded p-button-success p-mr-2" onClick={() => editcrew(rowData)} />
+            <Button icon="pi pi-trash" className="p-button-rounded p-button-warning" onClick={() => confirmDeletecrew(rowData)} />
+        </React.Fragment>
+    );
+}
 
   useEffect(async () => {
 
@@ -37,27 +54,22 @@ const CrewList = () => {
     }
   }, []);
 
-  const booleanChecker = (rowData, item) => {
-    if (typeof rowData[item.field] === "boolean") {
-      return ((rowData[item.field]) ? "Accepted" : "Unaccepted");
-    } else {
-      return rowData[item.field];
-    }
-  };
+ 
 
   const dynamicColumns = columns.map((col, i) => {
     return <Column 
         key={col.field} 
         field={col.field} 
         header={col.header}
-        body={booleanChecker} />;
+        sortable />;
   });
 
   return (
     <div>
       <div className="card">
-        <DataTable value={crews} responsiveLayout="scroll">
+        <DataTable value={crews} responsiveLayout="scroll" showGridlines scrollable scrollHeight="400px">
           {dynamicColumns}
+          <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: '8rem' }}></Column>
         </DataTable>
 
       </div>
