c37963165109ec95c258f70f1e741146c645e3b5
chuongtang <chuong.tang@aol.com>
2021-12-23 22:15:13 -0700

    moved 2 buttons from navbar to mainpage

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -60,19 +60,7 @@ const NavBar = () => {
     const leftContents = (
         <React.Fragment>
             <img alt="logo" src={Logo} height="40"></img>
-            {isAuthenticated && <div>
-                <Button type="button" label="Manage crew list" icon="pi pi-users" className="p-button-raised p-button-rounded p-button-warning p-button-text p-mx-4" onClick={() => setDisplayMaximizable(true)}>
-                </Button>
-                <Dialog visible={displayMaximizable} maximizable modal style={{ width: '80vw' }} onHide={() => onHide()}>
-                   <CrewList />
-                </Dialog>
-
-                <Button type="button" label="Create Time report" icon="pi pi-clock" className="p-button-raised p-button-rounded p-button-info p-button-text" onClick={() => setDisplayReportMaximizable(true)}>
-                </Button> 
-                <Dialog visible={displayReportMaximizable} maximizable modal style={{ width: '90vw' }} onHide={() => onHide()}>
-                   <GridBody />
-                </Dialog></div>
-                }
+           
         </React.Fragment>
     );
 
--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,41 @@ import NavBar from "../components/NavBar";
 import './App.css';
 import MainPage from "../components/MainPage";
 import CrewmemberList from '../components/CrewList/CrewmemberList';
-
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'primereact/button';
 
 
 
 function App() {
 
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    user,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
   return (
     <div>
       <NavBar />
-      <CrewmemberList />
+      {isAuthenticated && <div>
+        <Button type="button" label="Manage crew list" icon="pi pi-users" className="p-button-raised p-button-rounded p-button-warning p-button-text p-mx-4 mt-2" onClick={() => setDisplayMaximizable(true)}>
+        </Button>
+        {/* <Dialog visible={displayMaximizable} maximizable modal style={{ width: '80vw' }} onHide={() => onHide()}>
+          <CrewList />
+        </Dialog> */}
+
+        <Button type="button" label="Create Time report" icon="pi pi-clock" className="p-button-raised p-button-rounded p-button-info p-button-text" onClick={() => setDisplayReportMaximizable(true)}>
+        </Button>
+        {/* <Dialog visible={displayReportMaximizable} maximizable modal style={{ width: '90vw' }} onHide={() => onHide()}>
+          <GridBody />
+        </Dialog> */}
+        </div>
+      }
       <MainPage />
-     
+
     </div>
   )
 }
