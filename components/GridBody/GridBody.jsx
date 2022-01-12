fb9b05b181d78c1ff9edc22a9cbcf22260b6da43
chuongtang <chuong.tang@aol.com>
2022-01-12 15:28:26 -0700

    completed stage 1 of this app

--- components/GridBody/GridBody.jsx
+++ components/GridBody/GridBody.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Toast } from 'primereact/toast';
+import { Dialog } from 'primereact/dialog';
 import './GridBody.css';
 import { Calendar } from 'primereact/calendar';
 import { Dropdown } from 'primereact/dropdown';
@@ -10,8 +10,9 @@ import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import GenHourCol from './GenHourCol.js';
 import cellsGenerator from "./cellsGenerator.js";
-// import exportPdf from "./ExportPDF.js";
 
+
+import Spinner from "../../src/images/Spinner.svg";
 import sailor from "../../src/images/sailor.svg";
 import ship from "../../src/images/ship.svg";
 import html2pdf from "html2pdf.js";
@@ -22,17 +23,16 @@ const GridBody = ({ user }) => {
 
     const [products, setProducts] = useState([]);
     const [selectedCells, setSelectedCells] = useState(null);
-    const hourFields = ["00", ""]
+
     const [crews, setCrews] = useState([]);
     const [crew, setCrew] = useState({});
-    const toast = useRef(null);
+
     const date = new Date();
     const [dataMonth, setDataMonth] = useState(Date.now());
+    const [displaySpinner, setDisplaySpinner] = useState(false);
 
-    const [remarks, setRemarks] = useState({});
-    const [comment, setComment] = useState('');
     const [restTime24SelectedCells, setRestTime24SelectedCells] = useState({})
-    const [selectedCellsObj, setSelectedCellsObj] = useState({});
+
 
 
     let HourRows = GenHourCol();
@@ -63,23 +63,12 @@ const GridBody = ({ user }) => {
     }, []);
 
 
-
-    const onCellUnselect = (event) => {
-        toast.current.show({ severity: 'warn', summary: `Item Unselected In Product`, detail: `${toCapitalize(event.field)}: ${event.value}`, life: 3000 });
-    }
-
-    const toCapitalize = (str) => {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    }
-
     const cellClass = (selectState) => {
         return !selectState ? 'SelectedStyle' : ''
     }
 
     const onCellsSelected = (e) => {
-
         setSelectedCells(e.value)
-        console.table('CELLS Selected', e.value)
 
         // count the ⬇ property of an objects array 
         const countBy = (arr, prop) => arr.reduce((prev, curr) => ((prev[curr[prop]] = ++prev[curr[prop]] || 1), prev), {});
@@ -140,7 +129,10 @@ const GridBody = ({ user }) => {
     }
 
     const exportPdf = () => {
-
+        setDisplaySpinner(true);
+        setTimeout(() => {
+            setDisplaySpinner(false);
+        }, 3000);
         let element = document.getElementById('pdfContent');
         var opt = {
             margin: 0.1,
@@ -157,8 +149,10 @@ const GridBody = ({ user }) => {
     return (
         <>
             <Button type="button" icon="pi pi-file-pdf" onClick={exportPdf} style={{ float: "right" }} className="p-button-success p-button-raised p-button-text p-mr-6" label="export to PDF" data-pr-tooltip="PDF" />
+            <Dialog visible={displaySpinner} style={{ width: '25rem' }} closable={false}>
+                <object type="image/svg+xml" data={Spinner} alt="Spinner"></object>
+            </Dialog>
             <div className="datatable-selection" id="pdfContent">
-                <Toast ref={toast} />
 
                 <header className="p-d-flex p-jc-center" style={{ color: "#6366f1" }}>
                     <h1 className="p-mx-3"> Seafarers work hour records </h1>
@@ -228,7 +222,7 @@ const GridBody = ({ user }) => {
                         {/* 
                     <Column style={restTimetStyle} field="restHr-7day" header="Total Rest time in 7-day" editor={remarkEditor} onCellEditComplete={onCellEditComplete}></Column> */}
 
-                        <Column style={commentStyle} field="comment" header="Comment/Remark" editor={remarkEditor} onCellEditComplete={onCellEditComplete} disabledSelection ></Column>
+                        <Column style={commentStyle} field="comment"  header="Comment/Remark" editor={remarkEditor} onCellEditComplete={onCellEditComplete}  ></Column>
                     </DataTable>
                 </div>
                 <h5 className="p-d-flex p-jc-center p-text-italic" id="copyRight">Copyright (c) 2022_  <a href={'http://chuongtang.com'}>CT</a> </h5>
